cacfc6caf4251aa63a33037c39e3fac839894df6
Constantina <polyccon@gmail.com>
2017-07-21 00:20:11 +0100

    added test
    deleted files

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,13 +1,18 @@
-var object1 =[
-{
-hex: "#FF1493",
-name: "DeepPink"
-},
-{
-hex: "#00BFFF",
-name: "DeepSkyBlue"
-}
+var object1 = [{
+    hex: "#FF1493",
+    name: "DeepPink"
+  },
+  {
+    hex: "#00BFFF",
+    name: "DeepSkyBlue"
+  }
 ];
 
+var object2 = {
+  url: "/search/deep"
+}
 
-module.exports = {object1};
+module.exports = {
+  object1,
+  object2
+};
