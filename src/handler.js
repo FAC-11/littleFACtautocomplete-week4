91413d5630cd660274ccde108cbdd6e034d2f35c
Constantina <polyccon@gmail.com>
2017-07-20 11:59:22 +0100

    server added an error message

--- src/handler.js
+++ src/handler.js
@@ -6,43 +6,6 @@ const fs = require('fs');
 const path = require('path');
 const lib = require('../lib/lib.json');
 
-const handleHomeRoute = (request, response) => {
-  const filePath = path.join(__dirname, '..', 'public', 'index.html');
-
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      console.log(error);
-      response.writeHead(500, 'Content-Type: text/html');
-      response.end("<h1> Sorry, it's not looking good, please try again.</h1>");
-    } else {
-      response.writeHead(200, 'Content-Type: text/html');
-      response.end(file);
-    }
-  });
-}
-
-const handlePublic = (request, response, url) => {
-  const extension = url.split('.')[1];
-  const extensionType = {
-    html: 'text/html',
-    css: 'text/css',
-    js: 'application/javascript',
-    ico: 'image/x-icon'
-  };
-  const filePath = path.join(__dirname, '..', url);
-  fs.readFile(filePath, function(error, file) {
-    if (error) {
-      console.log(error);
-      response.writeHead(500, 'Content-Type: text/html');
-      response.end("<h1> Sorry, it's not looking good, please try again.</h1>");
-    } else {
-      response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
-      response.end(file);
-    }
-  });
-}
-
-
 //search checks if the input characters are parts of names in database. If so, as a result we will get an array of all matching names
 function search(word) {
   const lowerCaseWord = word.toLowerCase();
@@ -85,8 +48,8 @@ function handleHTMLCSSJS(req,res) {
   fs.readFile(filePath, function(error, file) {
     if (error) {
       console.log(error);
-      res.writeHead(404, 'Content-Type: text/html');
-      res.end("<h1>404 not found.</h1>");
+      res.writeHead(500, 'Content-Type: text/html');
+      res.end("<h1> Sorry, it's not looking good, please try again.</h1>");
     } else {
       res.writeHead(200, `Content-Type: ${extensionType[extension]}`);
       res.end(file);
--- src/router.js
+++ src/router.js
@@ -1,23 +1,7 @@
 //THis is where the functions are called
 // functions are defined in handler
 
-// const fs = require('fs');
-// const path = require('path');
-// const handlers = require('./handler')
-//
-// const router = (request, response) => {
-//   const url = request.url;
-//
-//   if (url === "/") {
-//     handlers.handleHomeRoute(request, response);
-//   } else if (url.indexOf('public') != -1) {
-//     handlers.handlePublic(request, response, url);
-//
-//   } else {
-//     response.writeHead(404, 'Content-Type: text/html');
-//     response.end('<h1>404 not found</h1>')
-//   }
-// }
+
 const handlers = require("./handler")
 
 function router(req, res) {
@@ -25,8 +9,10 @@ function router(req, res) {
   if (req.url.includes('/search')){
     return handlers.handleSearch(req,res)
   }
-
+ else if (req.url === "/" || req.url.includes('public')) {
   return handlers.handleHTMLCSSJS(req,res)
 }
-
+  res.writeHead(404, 'Content-Type: text/html');
+  res.end('<h1>404 not found</h1>')
+}
 module.exports = router;
