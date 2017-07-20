22d3f74e0eb64a6853bbb97c622179704da3f1bc
alinasolinas <azayneeva@gmail.com>
2017-07-20 14:46:40 +0100

    working backend

--- src/router.js
+++ src/router.js
@@ -9,10 +9,13 @@ function router(req, res) {
   if (req.url.includes('/search')){
     return handlers.handleSearch(req,res)
   }
- else if (req.url === "/" || req.url.includes('public')) {
-  return handlers.handleHTMLCSSJS(req,res)
-}
-  res.writeHead(404, 'Content-Type: text/html');
-  res.end('<h1>404 not found</h1>')
+ // else if (req.url==='/')  {
+   handlers.handleHTMLCSSJS(req,res);
+   // res.writeHead(200, 'Content-Type: application/json');
+   // res.end(JSON.stringify(responseObject));
+// } else {
+//   res.writeHead(404, 'Content-Type: text/html');
+//   res.end('<h1>404 not found</h1>')
+//   }
 }
 module.exports = router;
