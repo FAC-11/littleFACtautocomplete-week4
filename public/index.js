af9816c089228bc3098cd95afc5d48c5bcee2b08
ameliejyc <amelie.chan@gmail.com>
2017-07-20 15:37:01 +0100

    added listener function onclick for hex code colour change

--- public/index.js
+++ public/index.js
@@ -1,72 +1,36 @@
-var testObj = {
-  results: [{
-      name: "DarkRed",
-      hex: "#8B0000"
-    },
-    {
-      name: "IndianRed ",
-      hex: "#CD5C5C"
-    },
-    {
-      name: "MediumVioletRed",
-      hex: "#C71585"
-    },
-    {
-      name: "OrangeRed",
-      hex: "#FF4500"
-    },
-    {
-      name: "PaleVioletRed",
-      hex: "#DB7093"
-    },
-    {
-      name: "Red",
-      hex: "#FF0000"
-    }
-  ]
-}
-
 const userInput = document.getElementById("userSearch");
+const colourResults = document.getElementById('results');
 
 userInput.addEventListener('input', function(event) {
   getApi(urlBuilder(cleanInput(event.value)), appendData);
   event.preventDefault();
 });
 
+// appendData function creates new list elements for results and creates another event listener for clicks on each one to return background colour change
 function appendData(responseObj) {
-  userInput.innerHTML = '';
-  var colourResults = document.getElementById('results');
-
+  colourResults.innerHTML = '';
   responseObj.results.forEach(function(val) {
-
-    var colourNode = document.createElement("li");
+    const colourNode = document.createElement("li");
     colourNode.className = 'dropdown';
     colourNode.textContent = val.name;
     colourResults.appendChild(colourNode);
-    var hexCode = val.hex;
-    displayColours(hexCode);
+    const hexCode = val.hex;
+    colourResults.li.addEventListener('onclick', function(event) {
+      const backgroundNode = document.getElementById('change-background');
+      backgroundNode.setAttribute('style', 'background-color:' + hexCode)
+    })
   })
-
 };
 
-appendData(testObj);
-// This is where all the functions go that take the typed input and return data/functions
-
-function displayColours(hexCode) {
-  var backgroundNode = document.getElementById('change-background');
-  backgroundNode.setAttribute('style', 'background-color: ')
-}
-
 // cleanInput puts user unput to lower case, replaces unfamiliar characters and passes value to urlBuilder
-// function cleanInput(event.value) {
-//   var userCleanInput = event.value.toLowerCase().replace(/[^a-z]/g, "");
-//   return userCleanInput;
-// }
+function cleanInput(event.value) {
+  var userCleanInput = event.value.toLowerCase().replace(/[^a-z]/g, "");
+  return userCleanInput;
+}
 
 //urlBuilder creates the url using the clean user input
 function urlBuilder(userCleanInput) {
   var url = "http://localhost:4000/" + "search/" + userCleanInput;
-  // api();
 }
 
 // http request function
@@ -75,7 +39,7 @@ function getApi(url, cb) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var responseObj = JSON.parse(xhr.responseText);
-      displayColours(responseObj);
+      appendData(responseObj);
     }
   }
   xhr.open("GET", url, true);
