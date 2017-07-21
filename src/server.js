4c66f03031931d1378dc05890417c471ee94d512
Yahia <zaky20@hotmail.com>
2017-07-21 12:32:38 +0100

    amended server.js file for heroku

--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ const router = require('./router');
 // Takes a word and search for it in the database
 // Returns an array of all matching results
 // Search is case insensitive
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 4000;
 
-const port = 4000;
+
 const server = http.createServer(router).listen(port);
 
-console.log(`server is running on localhost:${port}`);
+console.log(`server is running on localhost:${host} ${port}`);
